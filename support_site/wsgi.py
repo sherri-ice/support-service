7e9d6c424e5794bca33bec641a17bdeb7a472ded
katetrina <sherri.ice7@gmail.com>
2022-03-05 02:45:41 +0300

    Created support app, added Django Rest framework to requirements.txt, renamed main site folder to support_site

--- support_site/wsgi.py
+++ support_site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for support_service project.
+WSGI config for support_site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'support_service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'support_site.settings')
 
 application = get_wsgi_application()
