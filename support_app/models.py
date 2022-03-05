80735b304dfd2f9320f9106662bcc6fd77621a47
katetrina <sherri.ice7@gmail.com>
2022-03-05 04:38:40 +0300

    Models for support app

--- support_app/models.py
+++ support_app/models.py
@@ -1,1 +1,30 @@
 from django.db import models
+
+
+class Message(models.Model):
+    text = models.TextField()
+    media_url = models.URLField()
+    send_date = models.DateTimeField(auto_now_add=True)
+    ticket_id = models.ForeignKey('Ticket', related_name="messages",
+                                  on_delete=models.CASCADE)
+    # owner_id
+
+
+class Ticket(models.Model):
+    class Status:
+        ACTIVE = 1
+        FROZEN = 2
+        CLOSED = 3
+        STATUS_CHOICES = (
+            (ACTIVE, 'Active'),
+            (FROZEN, 'Frozen'),
+            (CLOSED, 'Closed'),
+        )
+
+    title = models.CharField(max_length=255)
+    body_text = models.TextField()
+    status = models.IntegerField(choices=Status.STATUS_CHOICES,
+                                 default=Status.ACTIVE)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_edited_date = models.DateTimeField(auto_now=True)
+    # owner_id
