70d3cd4d3e833248ae3ab3d658381133245cfe09
katetrina <sherri.ice7@gmail.com>
2022-03-07 23:23:44 +0300

    User register

--- support_site/urls.py
+++ support_site/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from support_app import views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/api/tickets/', views.TicketListAPIView.as_view(), name='tickets'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(),
-         name='token_refresh'),
+    path('api/tickets/', views.TicketListAPIView.as_view(), name='tickets'),
+    path('api/', include('user_handler_app.urls', namespace='auth'))
 ]
--- user_handler_app/views.py
+++ user_handler_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serialisers import RegistrationSerializer
 
-# Create your views here.
+from .models import User
+
+
+class RegisterAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegistrationSerializer
+
