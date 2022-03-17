c748ca05cb9294cd5cda96a3a1d6cf8a11feb6b1
katetrina <sherri.ice7@gmail.com>
2022-03-17 14:27:34 +0300

    Ticket views: deleted unnecessary view class

--- support_app/urls.py
+++ support_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import TicketAPIView, TicketListAPIView, TicketAPICreateView
+from .views import TicketAPIView, TicketAPICreateView
 
 urlpatterns = [
-    path('tickets/', TicketListAPIView.as_view(), name='tickets'),
+    path('tickets/', TicketAPICreateView.as_view(), name='tickets'),
     path('tickets/<int:pk>/', TicketAPIView.as_view()),
     path('tickets/add/', TicketAPICreateView.as_view(), name='create_ticket')
 ]
--- support_app/views.py
+++ support_app/views.py
@@ -2,13 +2,7 @@ from rest_framework import generics
 from .models import Ticket
 from .serialisers import TicketSerializer
 from .permissions import IsOwnerOrAdmin
-from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
-
-
-class TicketListAPIView(generics.ListAPIView):
-    queryset = Ticket.objects.all()
-    serializer_class = TicketSerializer
-    permission_classes = [IsAdminUser, ]
+from rest_framework.permissions import IsAuthenticated
 
 
 class TicketAPIView(generics.RetrieveUpdateDestroyAPIView):
@@ -17,9 +11,16 @@ class TicketAPIView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrAdmin]
 
 
-class TicketAPICreateView(generics.CreateAPIView):
-    queryset = Ticket.objects.all()
+class TicketAPICreateView(generics.ListCreateAPIView):
     serializer_class = TicketSerializer
     permission_classes = [IsAuthenticated]
 
+    def perform_create(self, serializer):
+        serializer.save(onwer=self.request.user)
 
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Ticket.objects.all()
+        else:
+            return Ticket.objects.filter(owner=user)
