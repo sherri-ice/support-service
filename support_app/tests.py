ce31a6ba2095a7d8cc0ba0480b110d663288a37d
katetrina <sherri.ice7@gmail.com>
2022-03-21 10:08:06 +0300

    Ticket tests: get ticket

--- support_app/tests.py
+++ support_app/tests.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from user_handler_app.models import User
+from support_app.models import Ticket
 
 
 class TicketsAPITests(APITestCase):
@@ -17,7 +18,7 @@ class TicketsAPITests(APITestCase):
         self.token = response.data['access']
 
     def test_create_ticket(self):
-        url = reverse("create_ticket")
+        url = reverse('create_ticket')
         data = {
             'title': 'test',
             'body_text': 'test',
@@ -26,6 +27,14 @@ class TicketsAPITests(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Bearer  ' + self.token)
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_tickets(self):
+        Ticket.objects.create(title="Test", body_text="test");
+        url = reverse('tickets')
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer  ' + self.token)
+        response = self.client.get(url, format='json')
+        self.assertEqual(response)
+
 
     # get ticket by id: permissions and auth
     # ? get tickets last status by id
